b84149e5a95da2b7ec4a6bb3f8af9cc972d25cf3
Rahul Shivsharan <rahul.shivsharan@ril.com>
2016-10-12 00:24:24 +0530

    Demonstration of R.adjust 	R.adjust applies callback function to the element present at 	the index which we pass as argument.

--- practise01.js
+++ practise01.js
@@ -8,6 +8,7 @@ var R = require("ramda");
 //console.log(R.add(2,3));
 //console.log(R.add(2)(3));
 
+/*
 var mappedFn = R.addIndex(R.map);
 var myArray = [2,3,4,5]; 
 console.log(myArray);
@@ -16,4 +17,18 @@ var myArray = mappedFn(function(val,index){
 },myArray);
 
 console.log(myArray);
+*/
 
+var myArray = [2,3,4,5], index = 2;
+console.log(myArray);
+myArray = R.adjust(fn,index,myArray);
+function fn(num){ // fn get applied to the element present index value 
+	return 40 + num;
+}
+console.log(myArray);
+
+myArray = [12,23,34,55], index = 3;
+console.log(myArray);
+var adjustFn = R.adjust(fn);
+myArray = adjustFn(index,myArray);
+console.log(myArray);
