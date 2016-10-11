6a10cc46ccebc161ed268b9994a34270207e726d
Rahul Shivsharan <rahul.shivsharan@ril.com>
2016-10-12 00:16:17 +0530

    addIndex implementation
    Added addIndex to map R.map to function for list

--- practise01.js
+++ practise01.js
@@ -5,6 +5,15 @@ var R = require("ramda");
 //var curriedFn = R.replace("{name}",R.__,"Hello {name}, how are you ?");
 //console.log(curriedFn("Rahul"));
 
-console.log(R.add(2,3));
-console.log(R.add(2)(3));
+//console.log(R.add(2,3));
+//console.log(R.add(2)(3));
+
+var mappedFn = R.addIndex(R.map);
+var myArray = [2,3,4,5]; 
+console.log(myArray);
+var myArray = mappedFn(function(val,index){
+	return val * 3;
+},myArray);
+
+console.log(myArray);
 
