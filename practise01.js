09015ce6d119627f3763574376e7ca69872c1846
Rahul Shivsharan <rahul.shivsharan@ril.com>
2016-10-12 00:41:30 +0530

    demontration of R.all
    	Checks for all the conditions passed as callback for
    	a given collection

--- practise01.js
+++ practise01.js
@@ -19,6 +19,7 @@ var myArray = mappedFn(function(val,index){
 console.log(myArray);
 */
 
+/*
 var myArray = [2,3,4,5], index = 2;
 console.log(myArray);
 myArray = R.adjust(fn,index,myArray);
@@ -32,3 +33,31 @@ console.log(myArray);
 var adjustFn = R.adjust(fn);
 myArray = adjustFn(index,myArray);
 console.log(myArray);
+*/
+
+
+var myArray = [12,34,45,56];
+var allFn = R.all(fn)
+
+function fn(num){
+	return ((num % 2) === 0 || (num % 3) === 0);
+}
+
+console.log(allFn(myArray));
+
+
+myArray = [paramFn(2),paramFn(4),paramFn(6)];
+function paramFn(num){
+	var fn = function(){
+		return 10 + num;
+	}
+	return fn;
+}
+
+allFn = R.all(fnOne);
+
+function fnOne(f){
+	return (f() % 2) === 0;
+}
+
+console.log(allFn(myArray));
