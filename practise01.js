4c8ad40d5f34a333a1a28c2dc22bf52d66df1937
Rahul Shivsharan <rahul.shivsharan@ril.com>
2016-10-11 23:12:36 +0530

    add Numbers using add with one or two arguments

--- practise01.js
+++ practise01.js
@@ -2,6 +2,9 @@ var R = require("ramda");
 
 //console.log(R.add(2,3));
 
-var curriedFn = R.replace("{name}",R.__,"Hello {name}, how are you ?");
-console.log(curriedFn("Rahul"));
+//var curriedFn = R.replace("{name}",R.__,"Hello {name}, how are you ?");
+//console.log(curriedFn("Rahul"));
+
+console.log(R.add(2,3));
+console.log(R.add(2)(3));
 
